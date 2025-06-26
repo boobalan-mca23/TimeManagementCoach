4fef100d43a92a420f5634b62ad1932253a4d136
unknown <boobalan-mca23>
2025-06-27 05:10:07 +0530

    add toggle to hide table view

--- client/src/components/FourQuadrants/FourQuadrants.jsx
+++ client/src/components/FourQuadrants/FourQuadrants.jsx
@@ -4,18 +4,24 @@ import './FourQuadrants.css'
 import TaskForm from '../TaskForm/TaskForm';
 import Button from '@mui/material/Button';
 import { IoAddOutline } from "react-icons/io5";
+import Switch from '@mui/material/Switch';
 import { toast } from 'react-toastify'
-function FourQuadrants({ tasks, setTask }) {
+const label = { inputProps: { 'aria-label': 'Size switch demo' } };
+function FourQuadrants({ tasks, setTask,setHideTable }) {
   const color = ["#2196F3", "#F44336", "#000000", "#FF9800"];
   const [gridData, setGridData] = useState([]);
   const [open, setOpen] = useState(false)
-
+  const [switchChecked, setSwitchChecked] =useState(true);
 
   const handleTaskSave = (task) => {
     setTask(prev => [...prev, task]);
     toast.success('Task created')
   }
-
+  const handleSwitchChange = (event) => {
+    setSwitchChecked(event.target.checked);
+    console.log("Default Switch:", event.target.checked);
+    setHideTable(event.target.checked)
+  };
   useEffect(() => {
     const categorizeTasksByPriority = (tasks) => {
       console.log('tasks', tasks)
@@ -66,8 +72,14 @@ function FourQuadrants({ tasks, setTask }) {
         <Button variant="contained" style={{fontWeight:"bolder"}} onClick={() => setOpen(true)} >
           <IoAddOutline /> Add Task
         </Button>
-
+        <Switch
+        {...label}
+        checked={switchChecked}
+        onChange={handleSwitchChange}
+        size="medium"
+      />
       </div>
+      
       <div className='gird-content'>
           <p>Aim to focus on Important and Not Urgent tasks to avoid these becoming urgent. This is a key trait of highly productive people</p>
           <p>Reprioritize your tasks by changing due dates or priority to focus on what matters most.</p>
